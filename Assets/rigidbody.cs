bc1734c0181f5b9d3dacfef2665e22a97457a648
Alessiiiiii <alexisgor2000@hotmail.com>
2024-09-01 21:13:39 -0300

    New
    Actualizacion

--- Assets/rigidbody.cs
+++ Assets/rigidbody.cs
@@ -1,17 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class rigidbody : MonoBehaviour
 {
     public float speed;
     public float jumpForce;
     public Vector2 inputVector;
+    public Vector3 velocity;
     public Rigidbody Rigidbody;
+    public float velocityMagnitude;
+    public bool CanJump;
+    
     // Start is called before the first frame update
     void Start()
     {
         Rigidbody = GetComponent<Rigidbody>();
+        CanJump = true;
+        
     }
 
     // Update is called once per frame
@@ -21,10 +28,34 @@ public class rigidbody : MonoBehaviour
         inputVector.y = Input.GetAxis("Vertical");
 
         Rigidbody.AddForce(inputVector.x*speed,0f,inputVector.y*speed,ForceMode.Impulse);
-        if (Input.GetKeyDown(KeyCode.Space))
+       
+        velocity=Rigidbody.velocity;
+        velocityMagnitude=velocity.magnitude;
+        
+        if (Input.GetKeyDown(KeyCode.Space)&& CanJump)
+                
         {
 
             Rigidbody.AddForce(0f, jumpForce, 0f, ForceMode.Impulse);
+            CanJump = false;
+        }
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log("Choque cotra : " + collision.gameObject.name);
+
+        if (collision.gameObject.CompareTag("piso"))
+        {
+            CanJump = true;
+        }
+
+        if (collision.gameObject.CompareTag("KillZone"))
+        {
+            Debug.Log("Kill Mee");
+            SceneManager.LoadScene(0);
+        }
+        if (collision.gameObject.CompareTag("Goal"){
+            SceneManager.L
         }
     }
 }
